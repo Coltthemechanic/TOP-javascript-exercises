7e865b8c65e9e5c6cd31cfd1331d1ca3fafb9751
Colton Shirley <coltonshirley31@gmail.com>
2023-10-02 16:45:18 -0700

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    return array.filter(val => !args.includes(val));
+    
 
 };
 
